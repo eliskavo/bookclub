8edf9ef8458ccbf639e25303b632d06668c7dbbb
eliskavo <eliska.voova@gmail.com>
2024-05-12 14:57:13 +0200

    basics

---
+++ src/RecentBook.jsx
@@ -0,0 +1,14 @@
+import Gabrielle_Zevin_Tomorrow from './Gabrielle_Zevin_Tomorrow.jpg';
+
+export const RecentBook = () => {
+    return (
+        <div>
+            <h1>RecentBook:</h1>
+            <p>Chosen by: </p>
+            <h2>Tomorrow, and Tomorrow, and Tomorrow</h2>
+            <h3>by Gabrielle Zevin</h3>
+            <img src={Gabrielle_Zevin_Tomorrow} alt="Gabrielle_Zevin_Tomorrow" />
+            
+        </div>
+    );
+}

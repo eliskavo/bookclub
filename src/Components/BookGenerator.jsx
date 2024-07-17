eb107f79df7f56f83147a4cb963f51cb64f5223e
Thi Phuong Vo | Eliška <eliska.voova@gmail.com>
2024-07-17 22:36:35 +0200

    buttons-added, minor-styling-added

--- src/Components/BookGenerator.jsx
+++ src/Components/BookGenerator.jsx
@@ -11,6 +11,9 @@ export const BookGenerator = () => {
             <h1>{book.title}</h1>
             <p>{book.author}</p>
             <p>{book.member}</p>
+            <button 
+                className ="bg-inherit border-solid border-2 hover:bg-blue-500 text-gray-300 font-bold py-2 px-4 rounded-full m-8"
+            >Choose this book</button>
         </div>
     );
 
